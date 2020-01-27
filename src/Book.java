8410b7a022b573e6bfdaf5065ae6e050c115db74
Sebastian.Blazejewski <blazejewski.s.m@gmail.com>
2020-01-27 03:14:40 +0100

    Second commit

--- src/Book.java
+++ src/Book.java
@@ -1,12 +1,30 @@
 class Book {
-    String name;
-    String author;
-    String publisher;
-    String genre;
-    short pages;
-    float rating;
-    float price;
+    private final String NAME;
+    private final String AUTHOR;
+    private final String GENRE;
+    private final String PUBLISHER;
+    private final short PAGES;
+    private final float RATING;
+    private final float PRICE;
 
-    Book(){}
+    Book(String name, String author, String genre, String publisher, short pages, float price, float rating){
+        NAME = name;
+        AUTHOR = author;
+        GENRE = genre;
+        PUBLISHER = publisher;
+        PAGES = pages;
+        PRICE = price;
+        RATING = rating;
+    }
+
+    void printInfo(){
+        System.out.println("Name - " + NAME);
+        System.out.println("Author - " + AUTHOR);
+        System.out.println("Genre - " + GENRE);
+        System.out.println("Publisher - " + PUBLISHER);
+        System.out.println("Pages - " + PAGES);
+        System.out.println("Price - " + PRICE + "zł");
+        System.out.println("Rating - " + RATING + "/10\n");
+    }
 
 }
